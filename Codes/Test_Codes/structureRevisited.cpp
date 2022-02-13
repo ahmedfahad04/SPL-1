83e6c615fb48ccf02de076ba0781a1ce3f75c529
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-02-14 00:46:15 +0600

    Learning Structrue

--- Codes/Test_Codes/structureRevisited.cpp
+++ Codes/Test_Codes/structureRevisited.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 // structure is simply a "user defined data type"
 // also the value is stored in runtime, as the 
@@ -15,25 +16,41 @@ struct student{
 typedef struct str{
     char *data;
     int len;
-} string;
+
+} st;
+
+typedef int integer;
 
 int main(){
+
+    /*-------------------------------------BASIC OF STURCTURE----------------------------------*/
 
     int a;
 
     // here "struct Student" is a data type
-    struct student s1, s2;
+    struct student s1, s2, *ptr;
 
     s1.name = "Fahad";
+    s1.roll = 14;
     printf("\nROLL: %d, NAME: %s\n", s1.roll, s1.name);
 
     // we can also directly cope the content of one struct to another
     s2 = s1;
+    ptr = &s2;
 
-    string s;
+    st s;
     s.len = 21;
     s.data = "Istiaq Ahmed Fahad";
 
     printf("NAME: %s, Age: %d", s.data, s.len);
+    printf("\nNow I'm printing the name using structure pointer: %s\n", ptr->name);
+
+    /*-------------------------------------TYPEDEF----------------------------------*/
+
+    integer y = 10;
+    int z = 15;
+    cout << "SUMMATION IS: " << z+y << endl;
+
+    /*-------------------------------------STRUCTURE PADDING----------------------------------*/
     
 }
