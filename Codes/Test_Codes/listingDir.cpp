db6e4af3bb572c7cb4229012229914957c83da2c
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-02-20 21:48:21 +0600

    Working on Command Suggestion

--- Codes/Test_Codes/listingDir.cpp
+++ Codes/Test_Codes/listingDir.cpp
@@ -1,33 +1,152 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <bits/stdc++.h>
+#define DELETE -1
+#define INSERT -2
+#define SUBSTITUE -3
+#define UNCHANGED -4
+using namespace std;
 
-int main(){
+int c[100][100], b[100][100];
+int m, n;
+
+void ED(char x[], char y[], int row, int col)
+{
+
+    // initialize
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+
+            c[i][j] = 0;
+            b[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        c[0][i] = i;
+        b[0][i] = -2;
+    }
+    for (int i = 0; i < row; i++)
+    {
+        c[i][0] = i;
+        b[i][0] = -1;
+    }
+
+    // calculation
+    for (int i = 1; i < row; i++)
+    {
+
+        for (int j = 1; j < col; j++)
+        {
+
+            int subs = c[i - 1][j - 1];
+            int del = c[i - 1][j];
+            int insert = c[i][j - 1];
+
+            if (x[i] == y[j])
+            {
+                c[i][j] = c[i - 1][j - 1];
+                b[i][j] = UNCHANGED;
+            }
+
+            else
+            {
+
+                int m1 = min(subs, del);
+                int minimum = min(m1, insert);
+
+                // cout << x[i] << " " << y[j] << endl;
+                if (minimum == subs)
+                {
+
+                    // min = substitute
+                    c[i][j] = c[i - 1][j - 1] + 1;
+                    b[i][j] = SUBSTITUE;
+                    // cout << "SUBSTITUTE\n";
+                }
+
+                else if (minimum == c[i][j - 1])
+                {
+                    // min = insert
+                    c[i][j] = c[i][j - 1] + 1;
+                    b[i][j] = INSERT;
+                    // cout << "INSERT\n";
+                }
+
+                else
+                {
+
+                    // min = del
+                    c[i][j] = c[i - 1][j] + 1;
+                    b[i][j] = DELETE;
+                    // cout << "DEL\n";
+                }
+            }
+        }
+    }
+}
+
+void execute(string a, string d)
+{
+
+    m = a.size() + 1;
+    n = d.size() + 1;
+
+    char xx[m + 1], yy[n + 1];
+
+    xx[0] = ' ';
+    yy[0] = ' ';
+
+    for (int i = 1; i < m; i++)
+        xx[i] = a[i - 1];
+    for (int i = 1; i < n; i++)
+        yy[i] = d[i - 1];
+
+    // function call
+    ED(xx, yy, m, n);
+
+    if (c[m - 1][n - 1] <= 3)
+    {
+        cout << "Edit Distance: " << c[m - 1][n - 1] << " -- " << d << "\n\n";
+        cout << "BINGOOOOOO" << endl;
+    }
+}
+
+int main()
+{
 
     DIR *folder;
     struct dirent *files;
 
     char ch[100];
     int cnt = 0;
+    string s = "claer";
     printf("Enter directory path: ");
     scanf("%s", ch);
 
     folder = opendir(ch);
 
-    if(folder == NULL){
+    if (folder == NULL)
+    {
         perror("Unable to read the directory");
     }
 
-    else{
+    else
+    {
 
-        while(files = readdir(folder)){
+        while (files = readdir(folder))
+        {
             cnt++;
-            printf("> %s\n", files->d_name);
-            
+
+            execute(s, files->d_name);
+            // printf("> %s\n", files->d_name);
         }
 
         printf("Total Files: %d\n", cnt);
     }
 
     closedir(folder);
-
 }
