87afe4526f409196dc8a86e523d42e00afcd7426
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-03-23 11:01:27 +0600

    Working on piping and forking processes

---
+++ Coding Segment/debug/Features/process_and_pipes.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+
+   
+    int arr[] = {1, 2, 4, 5, 9, 1};
+    int size = sizeof(arr) / sizeof(int);
+    int start, end;
+
+    // file descriptor and pipes
+    int fd[2];
+    if(pipe(fd) == -1){
+        cout << "Pipeline error...\n";
+    }
+
+    // you have to create this fork call after declaring the pipe call. It's order must be maintained.
+    // you have to declare PIPE first
+    // then you've to FORK the process
+    int id = fork();
+
+    if (id == 0)
+    {
+        // child process
+        start = 0;
+        end = size / 2;
+    }
+    else
+    {
+        // parent process
+        start = size / 2;
+        end = size;
+    }
+
+    int sum = 0;
+    for (int i = start; i < end; i++)
+    { // here we've such terms that is dependent on child and parent process
+        sum += arr[i];
+    }
+
+
+
+    if (id == 0)
+    {
+        close(fd[0]);
+        char name[100];
+        scanf("%[^\n]s", name);
+        
+        // write the ans of sum from the child process in your PARENT process
+        if(write(fd[1], &name, sizeof(name)) == -1){
+            cout << "\nFailed to Write values";
+        }
+        close(fd[1]);
+    }
+    else
+    {
+        close(fd[1]);
+        int readFromChildProcess;
+        char fromChild[500];
+
+        
+        if(read(fd[0], &fromChild, sizeof(fromChild)) == -1){
+            cout << "\nFiled to read values";
+        }
+
+        cout << "\nNAME from Child is: " << fromChild << endl;
+        close(fd[0]);
+
+        // cout << "\nSummation got from the child process is: " << readFromChildProcess << endl;
+        // int newsum = sum + readFromChildProcess;
+        // cout << "\nNEW summation: " << newsum << endl;
+
+        
+    }
+
+    cout << "\nSummation: " << sum << endl;
+}
---
+++ Coding Segment/debug/Test_Codes/functionPointer.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int display(int (*ptr)(int, int), int a, int b);
+int sum (int a, int b);
+int sub (int a, int b);
+int multi (int a, int b);
+int div (int a, int b);
+
+
+int main(){
+
+    // this is a function pointer
+    int (*xx)(int, int) = sum;    
+    int s = xx(1,4);
+    printf("SUM: %d\n", s);
+
+
+
+    // here we're passing only the function address named "sum"
+    int ss = display(sum, 5, 6);    
+    printf("NEW SUM: %d\n", ss);
+
+    int sss = display(sub, 74, 62);
+    printf("SUBSTACTION: %d\n", sss);
+
+
+
+    // array of funciton pointer
+    int (*choice[10])(int, int) = {sum, sub, multi, div};
+    int x = 21, y = 7;
+    int chs;
+    
+    // printf("Enter your choice (0-3): ");
+    // scanf("%d", &chs);
+
+    // printf("RESULT IS: %d\n", (*choice[chs])(x,y));
+
+    for(int i=0; i<4; i++){
+        printf("RESULT IS: %d\n", (*choice[i])(x,y));
+    }
+
+}
+
+int sum (int a, int b){
+    return a+b;
+}
+
+int sub (int a, int b){
+    return a-b;
+}
+
+int multi (int a, int b){
+    return a*b;
+}
+
+int div (int a, int b){
+    if(b != 0) return a/b;
+}
+
+// this is called CALLBACK FUNCTION as it accepts other FUNCTION POINTERS as args
+int display(int (*ptr)(int, int), int a, int b){    
+    return ptr(a,b);   
+};
